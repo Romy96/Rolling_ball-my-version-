ba44ecf5a0d9478743ab735eb7991dc643580c64
ROMY\Romy96 <romybijkerk1996@gmail.com>
2016-11-10 13:12:30 +0100

    My version of Rolling ball game so far

---
+++ Assets/BallBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehaviour : MonoBehaviour {
+
+	public float Speed;
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3 (0, 0, -3);	
+		rb = GetComponent<Rigidbody> ();
+
+		rb.velocity = new Vector3 (0, 10, 0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			rb.velocity = new Vector3 (-5, 0, 0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.D)) {
+			rb.velocity = new Vector3 (5, 0, 0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rb.velocity = new Vector3 (0, 10, 0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.W)) {
+			rb.velocity = new Vector3 (0, 0, 5);
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			rb.velocity = new Vector3 (0, 0, -5);
+		}
+
+	}
+}
